8c7f87930ca2fa2483f76bfb097deb5d07af9252
andiharun29 <andiharunalrasyid14@gmail.com>
2024-11-21 21:06:57 +0700

    manambahkan alamat di data mahasiswa

---
+++ app/src/main/java/com/example/arsiyekturmvvm/model/DataMahasiswa.kt
@@ -0,0 +1,12 @@
+package com.example.arsiyekturmvvm.model
+
+import android.provider.ContactsContract.CommonDataKinds.Email
+
+data class DataMahasiswa(
+    val nama: String = "",
+    val gender: String = "",
+    val email: String = "",
+    val alamat: String = "",
+
+)
+
