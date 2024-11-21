940d430879ff0acfb3a8fc091c2518ed3febc2a1
andiharun29 <andiharunalrasyid14@gmail.com>
2024-11-21 21:08:54 +0700

    membuat object jenis kelamin "laki dan wanita"

---
+++ app/src/main/java/com/example/arsiyekturmvvm/model/JenisKelamin.kt
@@ -0,0 +1,10 @@
+package com.example.arsiyekturmvvm.model
+
+import com.example.arsiyekturmvvm.R
+
+object JenisKelamin {
+    val listJK = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
